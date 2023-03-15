4fc894b83599c7c3643ff3026354381ab7e08ab5
pawelparhomenko@gmail.com <pawelparhomenko@gmail.com>
2023-03-15 21:36:01 +0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      Hi
+    </>
+  );
+}
+
+export default App;
