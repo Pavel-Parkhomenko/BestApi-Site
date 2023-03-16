42257068485dae381d4d8f2d295a281539fb0651
pawelparhomenko@gmail.com <pawelparhomenko@gmail.com>
2023-03-16 21:38:20 +0300

    add products page and fix styles home page

---
+++ src/pages/Products/Data.jsx
@@ -0,0 +1,14 @@
+export const homeObjOne = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'View Our Products',
+    headline: 'Shop through our catalog of products',
+    // description: 'Our products services can be access worldwide through our partnership with 150 banks around the globe. If there are any issues, just issue a refund and we will process your request immediately.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aut voluptatum, fugit, nobis officiis sunt tenetur rem quos explicabo natus dolore necessitatibus quas labore.',
+    buttonLabel: 'Shop Now',
+    imgStart: '',
+    img: '/images/svg-products-1.svg',
+    alt: 'Credit Card',
+    pageDirect: 'pricing'
+  };
