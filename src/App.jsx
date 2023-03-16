ab548ee2f2df396a5fe244518c333c98a23edba9
pawelparhomenko@gmail.com <pawelparhomenko@gmail.com>
2023-03-16 18:03:10 +0300

    add components and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.scss';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home/Home';
+import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <>
-      Hi
-    </>
+    <Router>
+      <ScrollToTop />
+      <Navbar />
+           <Route path='/credit-card-app' exact>
+             <Redirect to="/" />
+           </Route>
+           <Route path='/' exact component={ Home } />
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/pages/Home/Data.jsx
@@ -0,0 +1,42 @@
+export const homeObjOne = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Product Spotlight',
+    headline: 'Unlimited Transaction with 5% cash back',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam ullam debitis amet, voluptates iusto, illo consequatur architecto modi quia, doloremque.',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: '/images/svg-home-1.svg',
+    alt: 'Credit Card display image',
+    pageDirect: 'pricing'
+};
+
+export const homeObjTwo = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Pay with Reward',
+    headline: 'Seamless rewards redemption',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet soluta ipsa quibusdam, repellendus voluptas ducimus libero expedita minus necessitatibus sint, natus voluptates error.',
+    buttonLabel: 'Sign Up Now',
+    imgStart: '',
+    img: '/images/svg-home-2.svg',
+    alt: 'Users mobile phone display image',
+    pageDirect: 'sign-in'
+  };
+
+export const homeObjThree = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Data Analyzer',
+    headline: 'Your transaction data is powerful',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum quae sapiente natus consequatur, animi sunt est veritatis reprehenderit expedita reiciendis.',
+    buttonLabel: 'Learn More',
+    imgStart: 'start',
+    img: '/images/svg-home-3.svg',
+    alt: 'Reward redemption display image',
+    pageDirect: 'services'
+  };
+  
