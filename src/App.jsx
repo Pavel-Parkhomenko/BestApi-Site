3b43c4639058c6beee6acafbcd2469b2ebc10802
pawelparhomenko@gmail.com <pawelparhomenko@gmail.com>
2023-03-16 20:07:40 +0300

    add pricing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.scss';
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home/Home';
+import Pricing from './pages/Pricing/Pricing';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
 
@@ -11,10 +12,13 @@ function App() {
     <Router>
       <ScrollToTop />
       <Navbar />
+        <Routes>
            <Route path='/credit-card-app' exact>
              <Redirect to="/" />
            </Route>
            <Route path='/' exact component={ Home } />
+           <Route path='/pricing' exact component={ Pricing } />
+        </Routes>
       <Footer />
     </Router>
   );
