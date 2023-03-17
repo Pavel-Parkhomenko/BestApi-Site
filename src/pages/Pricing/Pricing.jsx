27c2cd9dbed42447f3558f859d5f16170fc522b1
pawelparhomenko@gmail.com <pawelparhomenko@gmail.com>
2023-03-17 14:12:42 +0300

    fix pricing page and styles

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -10,7 +10,7 @@ import Button from '../../components/Button';
 function Pricing() {
     
     return (
-        <IconContext.Provider value={{color: '#3f3d56', size: 55}}>
+        <IconContext.Provider value={{color: '#3f3d56', size: 64}}>
             <div className="pricing">
                 <div className="pricing__wrapper">
                     <h1 className="pricing__heading">Pricing</h1>
@@ -22,13 +22,14 @@ function Pricing() {
                                     <FaFire />
                                 </div>
                                 <h3>Starter</h3>
+                                <h4>$8.99</h4>
                                 <p>per month</p>
                                 <ul className="pricing__container-features">
-                                    <li>250 Transactions</li>
+                                    <li>100 Transactions</li>
                                     <li>2% Cash Back</li>
                                     <li>$10,000 Limit</li>
                                 </ul>
-                                <Button buttonSize="btn--wide" buttonColor="red">
+                                <Button buttonSize="btn--wide" buttonColor="blue">
                                     Choose Plan
                                 </Button>
                             </div>
@@ -40,7 +41,8 @@ function Pricing() {
                                     <BsXDiamondFill />
                                 </div>
                                 <h3>Gold</h3>
-                                <h3>per month</h3>
+                                <h4>$29.99</h4>
+                                <p>per month</p>
                                 <ul className="pricing__container-features">
                                     <li>1000 Transactions</li>
                                     <li>3.5% Cash Back</li>
@@ -52,12 +54,13 @@ function Pricing() {
                             </div>
                         </Link>
 
-                        <Link to="" className="pricing__container-card">
+                        <Link to="#" className="pricing__container-card">
                             <div className="pricing__container-cardInfo">
                                 <div className="icon">
                                     <GiCutDiamond />
                                 </div>
                                 <h3>Diamond</h3>
+                                <h4>$99.99</h4>
                                 <p>per month</p>
                                 <ul className="pricing__container-features">
                                     <li>Unlimited Transactions</li>
