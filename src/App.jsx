41b1f967588ca1cd8e7a9191d275a535dc3581d0
pawelparhomenko@gmail.com <pawelparhomenko@gmail.com>
2023-03-17 12:34:58 +0300

    add singing page

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
+import Services from './pages/Services/Services';
 import Pricing from './pages/Pricing/Pricing';
+import SignIn from './pages/SignIn/SignIn';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
-import Services from './pages/Services/Services'
 
 function App() {
   return (
@@ -18,8 +20,10 @@ function App() {
              <Redirect to="/" />
            </Route>
            <Route path='/' exact component={ Home } />
-           <Route path='/pricing' exact component={ Pricing } />
+           <Route path='/products' exact component={ Products } />
            <Route path='/services' exact component={ Services } />
+           <Route path='/pricing' exact component={ Pricing } />
+           <Route path='/sign-in' exact component={ SignIn } />
         </Routes>
       <Footer />
     </Router>
